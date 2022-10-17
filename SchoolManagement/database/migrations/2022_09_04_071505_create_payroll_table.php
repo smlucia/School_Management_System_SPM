beec30a9541b436a4957a9cf51274b0bb984d122
lushiya <it20085858@my.sliit.lk>
2022-10-17 13:46:22 +0530

    Payroll model and migration created

---
+++ SchoolManagement/database/migrations/2022_09_04_071505_create_payroll_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payroll', function (Blueprint $table) {
+            $table->id();
+
+
+            $table->string("staff_name");
+            $table->string("designation");
+            $table->string("photo");
+            $table->string("month");
+            $table->integer("year");
+            $table->integer("salary");
+            $table->integer("allowance");
+            $table->integer("cost");
+            $table->integer("ntf");
+            $table->integer("etf");
+            $table->integer("netSalary");
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payroll');
+    }
+};
